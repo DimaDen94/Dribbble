1c81a6b3af71d0d7aa8a7745e28e336e46b94ad7
DimaDen94 <DimaDen94@MAIL.RU>
2017-01-13 20:16:22 +0200

    library

---
+++ app/src/main/java/com/jetruby/dribbble/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jetruby.dribbble.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jetruby.dribbble.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
